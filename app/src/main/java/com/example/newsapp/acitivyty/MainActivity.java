4884e022e5829acd1a9fc9e7a211705f47f42635
ChenYuelun <911172573@qq.com>
2017-06-02 21:05:29 +0800

    添加slidingMenu库，简单实现侧滑菜单

--- app/src/main/java/com/example/newsapp/acitivyty/MainActivity.java
+++ app/src/main/java/com/example/newsapp/acitivyty/MainActivity.java
@@ -4,12 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.newsapp.R;
+import com.example.newsapp.Utils.DensityUtil;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setBehindContentView(R.layout.layout_leftmenu);
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setBehindOffset(DensityUtil.dip2px(this,200));
     }
 }
