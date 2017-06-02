4a47c2da6278e7a07a776fba9919733f0735e85e
ChenYuelun <911172573@qq.com>
2017-06-02 21:29:15 +0800

    内容填充，两个fragment

--- app/src/main/java/com/example/newsapp/acitivyty/MainActivity.java
+++ app/src/main/java/com/example/newsapp/acitivyty/MainActivity.java
@@ -1,18 +1,38 @@
 package com.example.newsapp.acitivyty;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.newsapp.R;
 import com.example.newsapp.Utils.DensityUtil;
+import com.example.newsapp.fragment.ContentFragment;
+import com.example.newsapp.fragment.LeftMenuFragment;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
 public class MainActivity extends SlidingFragmentActivity {
 
+    public static final String CONTENT_TAG = "content_tag";
+    public static final String LEFTMENU_TAG = "leftmenu_tag";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initView();
+        initFragment();
+
+
+    }
+
+    private void initFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fl_content,new ContentFragment(), CONTENT_TAG);
+        ft.replace(R.id.fl_leftmenu,new LeftMenuFragment(), LEFTMENU_TAG);
+        ft.commit();
+    }
+
+    private void initView() {
         setContentView(R.layout.activity_main);
         setBehindContentView(R.layout.layout_leftmenu);
         SlidingMenu slidingMenu = getSlidingMenu();
---
+++ app/src/main/java/com/example/newsapp/fragment/LeftMenuFragment.java
@@ -0,0 +1,30 @@
+package com.example.newsapp.fragment;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.newsapp.base.BaseFragment;
+
+/**
+ * Created by chenyuelun on 2017/6/2.
+ */
+
+public class LeftMenuFragment extends BaseFragment {
+    TextView textView;
+    @Override
+    public View initView() {
+        textView = new TextView(context);
+        textView.setTextColor(Color.RED);
+        textView.setTextSize(30);
+        textView.setGravity(Gravity.CENTER);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("LeftMenuFragment");
+    }
+}
