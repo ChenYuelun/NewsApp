c398231872ab32d6fd3c671a38a7edadaac0ff0c
ChenYuelun <911172573@qq.com>
2017-06-04 21:15:48 +0800

    使用OkHttp3.0请求新闻数据，并用Gson解析数据

---
+++ app/src/main/java/com/example/newsapp/app/MaApplication.java
@@ -0,0 +1,30 @@
+package com.example.newsapp.app;
+
+import android.app.Application;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by chenyuelun on 2017/6/4.
+ */
+
+public class MaApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .addInterceptor(new LoggerInterceptor("TAG"))
+                .connectTimeout(5000L, TimeUnit.MILLISECONDS)
+                .readTimeout(5000L, TimeUnit.MILLISECONDS)
+                //其他配置
+                .build();
+
+        OkHttpUtils.initClient(okHttpClient);
+
+    }
+}
--- app/src/main/java/com/example/newsapp/pager/NewsPager.java
+++ app/src/main/java/com/example/newsapp/pager/NewsPager.java
@@ -6,7 +6,14 @@ import android.util.Log;
 import android.view.Gravity;
 import android.widget.TextView;
 
+import com.example.newsapp.Utils.ConstantUtils;
 import com.example.newsapp.base.BasePager;
+import com.example.newsapp.domain.NewsControlBean;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import okhttp3.Call;
 
 /**
  * Created by chenyuelun on 2017/6/2.
@@ -33,5 +40,40 @@ public class NewsPager extends BasePager {
 
         fl_content.addView(textView);
 
+        getDataFromNet();
+
     }
+
+    private void getDataFromNet() {
+        String url = ConstantUtils.NEWSCENTER_PAGER_URL;
+        OkHttpUtils
+                .get()
+                .url(url)
+//                .addParams("username", "hyman")
+//                .addParams("password", "123")
+                .build()
+                .execute(new StringCallback()
+                {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("TAG","请求失败=="+e.getMessage());
+
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("TAG","请求成功=="+response);
+                        processData(response);
+                    }
+
+
+                });
+    }
+
+    private void processData(String json) {
+        NewsControlBean newsControlBean = new Gson().fromJson(json, NewsControlBean.class);
+        Log.e("TAG","数据解析成功:+" + newsControlBean.getData().get(0).getTitle());
+    }
+
+
 }
