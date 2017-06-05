63144db6905181d2a5c34b220ccddfbf9a8fec17
ChenYuelun <911172573@qq.com>
2017-06-05 20:48:21 +0800

    联网请求新闻详情页面新闻数据并尝试解析

--- app/src/main/java/com/example/newsapp/MenuDetailPager/TabDetaiPager.java
+++ app/src/main/java/com/example/newsapp/MenuDetailPager/TabDetaiPager.java
@@ -2,6 +2,7 @@ package com.example.newsapp.MenuDetailPager;
 
 import android.content.Context;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -11,9 +12,16 @@ import com.example.newsapp.R;
 import com.example.newsapp.Utils.ConstantUtils;
 import com.example.newsapp.base.MenuDetailBasePager;
 import com.example.newsapp.domain.NewsControlBean;
+import com.example.newsapp.domain.NewsDetailBean;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Call;
 
 /**
  * Created by chenyuelun on 2017/6/5.
@@ -29,6 +37,7 @@ public class TabDetaiPager extends MenuDetailBasePager {
     @BindView(R.id.viewpager_topNews)
     ViewPager viewpagerTopNews;
     private NewsControlBean.DataBean.ChildrenBean childrenBean;
+    private List<NewsDetailBean.DataBean.TopnewsBean> topnews;
 
     public TabDetaiPager(Context context, NewsControlBean.DataBean.ChildrenBean childrenBean) {
         super(context);
@@ -46,9 +55,37 @@ public class TabDetaiPager extends MenuDetailBasePager {
     @Override
     public void initData() {
         super.initData();
-        topNewsTitle.setText(childrenBean.getTitle());
+//        topNewsTitle.setText(childrenBean.getTitle());
+        String url = ConstantUtils.BASE_URL + childrenBean.getUrl();
+        getDataFromNet(url);
+
+    }
+
+    private void getDataFromNet(String url) {
+        OkHttpUtils
+                .get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("TAG", "请求失败==" + e.getMessage());
+
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("TAG", "请求成功==" + response);
+                        processData(response);
+                    }
 
 
+                });
+    }
+
+    private void processData(String json) {
+        NewsDetailBean newsDetailBean = new Gson().fromJson(json, NewsDetailBean.class);
+        topnews = newsDetailBean.getData().getTopnews();
     }
 
 
