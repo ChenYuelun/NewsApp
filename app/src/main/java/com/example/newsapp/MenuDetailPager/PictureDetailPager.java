4bf9e8dd67dcaeeb39a73e31d69da995cb62f379
ChenYuelun <911172573@qq.com>
2017-06-06 21:53:42 +0800

    使用官方下拉刷新控件

--- app/src/main/java/com/example/newsapp/MenuDetailPager/PictureDetailPager.java
+++ app/src/main/java/com/example/newsapp/MenuDetailPager/PictureDetailPager.java
@@ -1,6 +1,7 @@
 package com.example.newsapp.MenuDetailPager;
 
 import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
@@ -8,7 +9,6 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
-import com.example.myutils_library.Utils.CacheUtils;
 import com.example.myutils_library.Utils.ConstantUtils;
 import com.example.newsapp.R;
 import com.example.newsapp.adapter.MyRecyclerViewAdapter;
@@ -35,9 +35,12 @@ public class PictureDetailPager extends MenuDetailBasePager {
     RecyclerView recyclerView;
     @BindView(R.id.pb_picture)
     ProgressBar pbPicture;
+    @BindView(R.id.swipeRefresh)
+    SwipeRefreshLayout swipeRefresh;
     private List<PictureNewsBean.DataBean.NewsBean> newsBeanList;
     private MyRecyclerViewAdapter adapter;
     private StaggeredGridLayoutManager layoutManger;
+    private String moreUrl;
 
     public PictureDetailPager(Context context, NewsControlBean.DataBean dataBean) {
         super(context);
@@ -48,6 +51,12 @@ public class PictureDetailPager extends MenuDetailBasePager {
     public View initView() {
         View view = View.inflate(context, R.layout.pager_picture_detail, null);
         ButterKnife.bind(this, view);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getDataFromNet(moreUrl);
+            }
+        });
         return view;
     }
 
@@ -74,32 +83,48 @@ public class PictureDetailPager extends MenuDetailBasePager {
                     public void onResponse(String response, int id) {
                         Log.e("TAG", "组图请求成功");
                         processData(response);
+                        swipeRefresh.setRefreshing(false);
                     }
 
 
                 });
     }
 
+    private boolean isFirstGet = true;
+
     private void processData(String json) {
         PictureNewsBean pictureNewsBean = new Gson().fromJson(json, PictureNewsBean.class);
-
-        if (pictureNewsBean != null) {
-            pbPicture.setVisibility(View.GONE);
-            newsBeanList = pictureNewsBean.getData().getNews();
-            adapter = new MyRecyclerViewAdapter(context, newsBeanList);
-            recyclerView.setAdapter(adapter);
-            layoutManger = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
-            recyclerView.setLayoutManager(layoutManger);
+        String more = pictureNewsBean.getData().getMore();
+        if (more != null) {
+            moreUrl = ConstantUtils.BASE_URL + more;
         }
+        if (pictureNewsBean != null) {
+            if (isFirstGet) {
+                isFirstGet = false;
+                pbPicture.setVisibility(View.GONE);
+                newsBeanList = pictureNewsBean.getData().getNews();
+                adapter = new MyRecyclerViewAdapter(context, newsBeanList);
+                recyclerView.setAdapter(adapter);
+                layoutManger = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+                recyclerView.setLayoutManager(layoutManger);
+            }else {
+                newsBeanList.addAll(0,pictureNewsBean.getData().getNews());
+                adapter.notifyDataSetChanged();
+            }
+
+        }
+
+
     }
 
     private boolean isShowList = true;
+
     public void switchListOrGrid(ImageButton ib_switch_list_grid) {
-        if(isShowList) {
+        if (isShowList) {
             isShowList = false;
             layoutManger.setSpanCount(2);
             ib_switch_list_grid.setImageResource(R.drawable.icon_pic_list_type);
-        }else {
+        } else {
             isShowList = true;
             layoutManger.setSpanCount(1);
             ib_switch_list_grid.setImageResource(R.drawable.icon_pic_grid_type);
